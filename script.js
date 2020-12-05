997914ef0b3ada26f6e6b4deb5a2d601da45b007
Vlad Udovychenko <rafull6@gmail.com>
2020-12-05 11:34:20 +0100

    Add js

---
+++ script.js
@@ -0,0 +1,11 @@
+var first = 5;
+var second = 'John';
+
+var result = first * second;
+
+// var name = 'John';
+// var surname = 'Doe';
+
+// var fullName = name + ' ' + surname;
+
+console.log(result);
